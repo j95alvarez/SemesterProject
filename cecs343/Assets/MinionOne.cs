db9809d253a125d0c9040b48ec57eeb68f100797
j95alvarez <juanalvarez391@yahoo.com>
2016-09-01 10:43:09 -0700

    Added Enemy

---
+++ cecs343/Assets/MinionOne.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MinionOne : MonoBehaviour {
+    public float speed;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(-(speed * Time.deltaTime), 0, 0);
+    }
+}
