d76f4f2789706538897f86ce7619abc3068a9054
j95alvarez <juanalvarez391@yahoo.com>
2016-09-04 07:30:38 -0700

    Added an Attack button
    Player can attack an enemy when in range.

--- cecs343/Assets/Scripts/PlayerMovement.cs
+++ cecs343/Assets/Scripts/PlayerMovement.cs
@@ -1,14 +1,13 @@
-﻿using UnityEngine;
+﻿    using UnityEngine;
 using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
     public float speed;
     public float jump;
-    
-    
+    public GameObject target, prefab;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 	
 	}
 	
@@ -37,6 +36,29 @@ public class PlayerMovement : MonoBehaviour {
         {
             Debug.Log("Right");
             transform.Translate((speed * Time.deltaTime), 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            Debug.Log("Attack");
+            attack();
+        }
+    }
+
+    void attack ()
+    {
+        // create a variable called distance to make sure the distance is close enough
+        // to inflict damage from a melee attack target.transform.positon = the position
+        // of the object to attack and transform.position is the position of the players
+        // transform
+        float distance = Vector3.Distance(target.transform.position, transform.position);
+
+        // make sure the distance is not greater than 1.5 so the player cannot
+        // melee an enemy from to far away
+        if (distance < 1.5f)
+        {
+            Debug.Log("Hit");
+            Instantiate(prefab, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
         }
     }
 }
