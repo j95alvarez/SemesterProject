27cffd68174c0a1ec60ab0aaa38a594b17ee0ffe
TCanet <tcanet94@gmail.com>
2016-09-06 08:31:45 -0700

    Enemy ai
    The enemy now follows the player and when runs into the the player takes
    away the players health. After attacking the enemy stops moving for a
    second so that the player can run or retaliate

---
+++ cecs343/Assets/EnemyA1.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyA1 : MonoBehaviour 
+{
+	public GameObject player;
+	public float speed = .5F;
+	PlayerStatus p;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.position = Vector3.MoveTowards (transform.position, player.transform.position, speed * Time.deltaTime);
+	}
+
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		//Debug.Log ("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+		if (col.gameObject.name == "Player") 
+		{
+			p = col.gameObject.GetComponent<PlayerStatus> ();
+			p.health -= 20;
+			speed = 0;
+
+			StartCoroutine (resetSpeed());
+
+		}
+	}
+
+	IEnumerator resetSpeed()
+	{
+		yield return new WaitForSeconds (1);
+		speed = .5f;
+	}
+
+
+}
--- cecs343/Assets/Scripts/PlayerMovement.cs
+++ cecs343/Assets/Scripts/PlayerMovement.cs
@@ -17,25 +17,25 @@ public class PlayerMovement : MonoBehaviour {
         
         if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.Space))
         {
-            Debug.Log("UP");
+            //Debug.Log("UP");
             transform.Translate(0, (jump * speed * Time.deltaTime), 0);
         }
 
         if (Input.GetKey(KeyCode.DownArrow))
         {
-            Debug.Log("Down");
+            //Debug.Log("Down");
             transform.Translate(0, -(speed * Time.deltaTime), 0);
         }
 
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            Debug.Log("Left");
+            //Debug.Log("Left");
             transform.Translate(-(speed * Time.deltaTime), 0, 0);
         }
 
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            Debug.Log("Right");
+            //Debug.Log("Right");
             transform.Translate((speed * Time.deltaTime), 0, 0);
         }
     }
