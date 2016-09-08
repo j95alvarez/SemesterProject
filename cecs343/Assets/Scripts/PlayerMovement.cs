bd348dd874dd5ca3cf4a40a6b139d1b24e3ca375
j95alvarez <juanalvarez391@yahoo.com>
2016-09-08 15:36:41 -0700

    Fliping fixes
    Fixed Flipping

--- cecs343/Assets/Scripts/PlayerMovement.cs
+++ cecs343/Assets/Scripts/PlayerMovement.cs
@@ -1,12 +1,12 @@
-﻿    using UnityEngine;
+﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovement : MonoBehaviour
+{
     public float speed;
     public float jump;
     public GameObject target, prefab;
-    public bool isClimbing;
-	public bool climb;
+    public bool NeedRev = false;
 
     private int pHealth = 100;
 
@@ -15,55 +15,87 @@ public class PlayerMovement : MonoBehaviour {
 
     public float offest;
     // Use this for initialization
-    void Start () {
-        isClimbing = false;
-		climb = false;
-	}
+    void Start()
+    {
 
-	// Update is called once per frame
-	void Update () {
-        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //Moving Direction Detacter
+        //Use NeedRev to determin does the 
+        //left right key need to reverse
+        if (Input.GetAxis("Horizontal") > 0.1)
+        {
+            //Debug.Log("Pos");
+            transform.localScale = new Vector2(1, 1);
+            NeedRev = false;
+        }
+        else if (Input.GetAxis("Horizontal") < -0.1)
+        {
+            //Debug.Log("Neg");
+            transform.localScale = new Vector2(-1, 1);
+            NeedRev = true;
+        }
+
         // Basic player movement and 
-        if (Input.GetKey(KeyCode.Space) && !isClimbing) {
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.Space))
+        {
             //Debug.Log("UP");
             VerticalMove(jump * speed * Time.deltaTime);
         }
 
-		if (Input.GetKey(KeyCode.LeftArrow)) {
-            //Debug.Log("Left");
-            HorizontalMove(-(speed * Time.deltaTime));
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            if (NeedRev)//while player change face direction
+            {
+                Debug.Log("reverse1");
+                HorizontalMove((speed * Time.deltaTime));
+            }
+            else
+            {
+                //Debug.Log("Left");
+                HorizontalMove(-(speed * Time.deltaTime));
+            }
         }
 
-	    if (Input.GetKey(KeyCode.RightArrow)) {
-            //Debug.Log("Right");
-            HorizontalMove(speed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            if (NeedRev)
+            {
+                Debug.Log("reverse2");
+                HorizontalMove(-(speed * Time.deltaTime));
+            }
+            else
+            {
+                //Debug.Log("Right");
+                HorizontalMove(speed * Time.deltaTime);
+            }
+
         }
 
-        if (Input.GetKey(KeyCode.A) && !isClimbing) {
+        if (Input.GetKey(KeyCode.A))
+        {
             //Debug.Log("Attack");
             Attack();
         }
 
-        if (climb) {
-            if (Input.GetKey(KeyCode.UpArrow)) {
-                VerticalMove(speed * Time.deltaTime);
-            }
-
-            if (Input.GetKey(KeyCode.DownArrow)) {
-                VerticalMove(-(speed * Time.deltaTime));
-            }
-        }
     }
 
-    void HorizontalMove(float amount) {
+    void HorizontalMove(float amount)
+    {
         transform.Translate(amount, 0, 0);
     }
 
-    void VerticalMove(float amount) {
+    void VerticalMove(float amount)
+    {
         transform.Translate(0, amount, 0);
     }
 
-    void Attack () {
+    void Attack()
+    {
         // create a variable called distance to make sure the distance is close enough
         // to inflict damage from a melee attack target.transform.positon = the position
         // of the object to attack and transform.position is the position of the players
@@ -72,33 +104,33 @@ public class PlayerMovement : MonoBehaviour {
 
         // make sure the distance is not greater than 1.5 so the player cannot
         // melee an enemy from to far away
-        if (distance < 1.5f) {
+        if (distance < 1.5f)
+        {
             Debug.Log("Hit");
             nasd = Instantiate(prefab, new Vector3(transform.position.x + offest, transform.position.y, transform.position.z), Quaternion.identity);
-
-            if (target.GetComponent<EnemyAI>().GetComponent<PlayerStatus>().health > 0) {
-                target.GetComponent<EnemyAI>().GetComponent<PlayerStatus>().health -= 10;
-            } else {
-                Destroy(target);
-            }
         }
 
         StartCoroutine(Wait(3.0F));
     }
 
-    IEnumerator Wait(float waitTime) {
+    IEnumerator Wait(float waitTime)
+    {
         yield return new WaitForSeconds(waitTime);
         //print("WaitAndPrint " + Time.time);
         Destroy(nasd);
     }
 
 
-    void OnCollisionEnter2D(Collision2D collision) {
-        if (collision.gameObject.name == "Enemy 1") {
-            if (pHealth > 0) {
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Enemy 1")
+        {
+            if (pHealth > 0)
+            {
                 pHealth -= 10;
             }
-            Debug.Log("phealth is: " + pHealth);
         }
-    } 
+
+        Debug.Log("phealth is: " + pHealth);
+    }
 }
