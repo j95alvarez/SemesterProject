a3de96a84481cef1956f31b7294c6ab1a01107fd
ChenMT <gene31105@gmail.com>
2016-09-11 19:54:18 -0700

    spawn on players pos
    still have bug

---
+++ cecs343/Assets/Scripts/Spawn.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawn : MonoBehaviour {
+    public float setDTime;
+    public float time;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //count second.
+        time += Time.deltaTime;
+        if (time > setDTime)
+        {
+            SpawnEnemy();
+            time = 0;
+        }
+    }
+
+    void SpawnEnemy()
+    {
+        Vector3 Range = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        Debug.Log("Spawn !");
+        Instantiate(GameObject.Find("Enemy 1"), Range, transform.rotation);
+    }
+}
