5c6590d62006175620372fd5c5b673843621087d
ChenMT <gene31105@gmail.com>
2016-09-14 11:47:31 -0700

    First Commit
    First Commit and few setup

---
+++ cecs343/Assets/Boss.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boss : MonoBehaviour {
+
+    public float speed = .5F;
+    public int eHealth;
+
+    public float counter;
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, GameObject.Find("Player").transform.position, speed * Time.deltaTime);
+        //Debug.Log(GameObject.Find("Player").transform.position);
+        if (eHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.name == "Player")
+        {
+            col.gameObject.GetComponent<PlayerMovement>().pHealth -= 10;
+            speed = 0;
+            StartCoroutine(resetSpeed());
+        }
+    }
+    IEnumerator resetSpeed()
+    {
+        yield return new WaitForSeconds(1);
+        speed = .5f;
+    }
+}
