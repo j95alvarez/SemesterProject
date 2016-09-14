ada4c380faaa1376f8f881b3fdbc6d9761e06101
kriscubie <kriscubie@yahoo.com>
2016-09-14 14:49:41 -0700

    Second Minon
    Does 5 more damage than first minion

---
+++ cecs343/Assets/Scripts/MinionTwo.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MinonTwo : MonoBehaviour
+{
+    public GameObject player;
+    PlayerStatus p;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        //Debug.Log ("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+        if (col.gameObject.name == "Player")
+        {
+            p = col.gameObject.GetComponent<PlayerStatus>();
+            speed = 0;
+
+            StartCoroutine(resetSpeed());
+
+        }
+    }
+
+    IEnumerator resetSpeed()
+    {
+        yield return new WaitForSeconds(1);
+    }
+}
