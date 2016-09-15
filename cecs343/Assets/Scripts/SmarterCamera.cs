4db584890518c5f009c13fe8a3937cb933b6325e
Khoax <danny.chau.lb@gmail.com>
2016-09-14 21:56:59 -0700

    Basic framework done.
    Main roadblock: Can't figure out why the current position vector won't
    update every frame.

---
+++ cecs343/Assets/Scripts/SmarterCamera.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SmarterCamera : MonoBehaviour {
+
+    public GameObject player;
+    public float xThreshold, yThreshold;
+    public int followSpeed;
+    public float currentX;
+    public double what;
+    public Vector3 position;
+
+	// Use this for initialization
+	void Start () {
+        Camera.main.orthographicSize = 5;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        position = player.transform.position;
+
+        currentX = position.x;
+        //if(position.x < 1.0/xThreshold) {
+        //    transform.Translate(followSpeed*-1*Time.deltaTime, 0, 0);
+        //    transform.Translate(1, 0, 0);
+        //}
+        what = 1.0 / xThreshold;
+
+        //transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -1);
+    }
+}
