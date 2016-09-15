dc5c82fd97645865938a4a48c58bb86022d0e96f
kriscubie <kriscubie@yahoo.com>
2016-09-15 12:49:36 -0700

    Minion TWo
    Faster and does more damage by 5

---
+++ cecs343/Assets/Scripts/MinionTwo.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MinionTwo : MonoBehaviour
+{
+    public float speed = .9F;
+    public int eHealth;
+    public GameObject spawn;
+    public int DistanceToDelete;
+    public float ChkDistInterval;
+
+    public float counter;
+    // Use this for initialization
+    void Start()
+    {
+        //playermove = GetComponent<PlayerMovement>();
+        spawn = GameObject.Find("Controller");
+    }
+
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, GameObject.Find("Player").transform.position, speed * Time.deltaTime);
+        //Debug.Log(GameObject.Find("Player").transform.position);
+        counter += Time.deltaTime;
+        if (counter >= ChkDistInterval)
+        {
+            ChkDist();
+            //Debug.Log("Check Point");
+            counter = 0;
+        }
+        if (eHealth <= 0)
+        {
+            spawn.gameObject.GetComponent<Spawn>().SpawnCounter--;
+            Destroy(gameObject);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.name == "Player")
+        {
+            col.gameObject.GetComponent<PlayerMovement>().pHealth -= 15;
+            speed = 0;
+            StartCoroutine(resetSpeed());
+        }
+    }
+    void ChkDist()
+    {
+        //Debug.Log("CK CK");
+        if (Vector3.Distance(GameObject.Find("Player").transform.position, transform.position) > DistanceToDelete)
+        {
+            spawn.gameObject.GetComponent<Spawn>().SpawnCounter--;
+            Destroy(gameObject);
+            //Debug.Log("Too Far");
+        }
+    }
+
+    IEnumerator resetSpeed()
+    {
+        yield return new WaitForSeconds(1);
+        speed = .9F;
+    }
+}
