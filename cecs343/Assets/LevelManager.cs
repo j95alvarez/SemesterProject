b71452fdd9b6ca270775a62d940ce8b80f6ce0e5
j95alvarez <juanalvarez391@yahoo.com>
2016-11-21 11:44:43 -0800

    Main Menu
    Created a main menu that plays the game, opens an options menu or quits
    the game (quits works when its clicked on the build, not in unity)

---
+++ cecs343/Assets/LevelManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+    public Transform mainMenu, optionsMenu;
+
+    public void LoadScene(string game) {
+        Application.LoadLevel(game);
+    }
+
+    public void QuitGame() {
+        Application.Quit();
+    }
+
+    public void OptionsMenu(bool clicked) {
+        if (clicked == true)
+        {
+            optionsMenu.gameObject.SetActive(clicked);
+            mainMenu.gameObject.SetActive(!clicked);
+        }
+        else
+        {
+            optionsMenu.gameObject.SetActive(clicked);
+            mainMenu.gameObject.SetActive(!clicked);
+        }
+    }
+}
