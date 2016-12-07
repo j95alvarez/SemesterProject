af90c0b8f8f2c027bc95beed2f300d3b08d499f5
Khoax <danny.chau.lb@gmail.com>
2016-12-07 00:17:25 -0800

    Fixed and better camera.
    Instantiate: Smoothcoeff  : 1
    Fast coeff : .4
    Fast Distance: 30
    STart Zoom: 5
    x Threshold: 3.5
    y Threshhold: 3.5

--- cecs343/Assets/Scripts/SmarterCamera.cs
+++ cecs343/Assets/Scripts/SmarterCamera.cs
@@ -5,16 +5,15 @@ public class SmarterCamera : MonoBehaviour {
 
     public GameObject player;
     public float xThreshold, yThreshold;
-    public int maxZoom, minZoom;
-    public float smoothCoeff, followSpeed;
+    public int maxZoom, minZoom, startZoom;
+    public float smoothCoeff, fastCoeff, fastDistance;
 
     private Vector3 velocity = Vector3.zero;
 
     // Use this for initialization
     void Start () {
-        Camera.main.orthographicSize = 4;
+        Camera.main.orthographicSize = startZoom;
         //transform.position = (player.transform.position + new Vector3(0,0,-10)); // Move camera on top of player to start.
-        followSpeed = 1*GameObject.Find("Player").GetComponent<PlayerMovement>().speed;
 	}
 	
 	// Update is called once per frame
@@ -22,10 +21,16 @@ public class SmarterCamera : MonoBehaviour {
         var viewPosition = Camera.main.WorldToViewportPoint(player.transform.position);
         var position = player.transform.position;
         position.z = transform.position.z;
+        position.y += 2;
 
         if(viewPosition.x < 1.0/xThreshold || viewPosition.x > (xThreshold-1)/xThreshold || viewPosition.y < 1.0 / yThreshold || viewPosition.y > (yThreshold-1)/yThreshold)
         {
-            transform.position = Vector3.SmoothDamp(transform.position, position, ref velocity, smoothCoeff);
+            Debug.Log(Vector3.Distance(viewPosition, position));
+            if (Mathf.Abs(Vector3.Distance(viewPosition, position)) > fastDistance)
+            {
+                transform.position = Vector3.SmoothDamp(transform.position, position, ref velocity, fastCoeff);
+            }
+            else transform.position = Vector3.SmoothDamp(transform.position, position, ref velocity, smoothCoeff);
         }
 
         if(Input.GetKeyDown(KeyCode.Equals)) zoomIn();
